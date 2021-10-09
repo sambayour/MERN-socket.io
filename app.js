e7a167d74d8d248bbf7d41a6f8bb338772a923f2
azucar_muchacho <samuelolubayo@gmail.com>
2021-10-09 18:55:31 +0100

    ejs

--- app.js
+++ app.js
@@ -2,12 +2,21 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.listen(3000);
 
-app.get('/',(req, res) => {
-res.send('<h3>Hello World</h3>')
+app.get('/',(req, res) => { 
+    // res.sendFile('./views/index.html',{root: __dirname})
+    res.render('index');
 })
 
-app.get('/api',(req, res) => {
-    res.send('<p>Api route</p>')
+app.get('/api',(rneq, res) => {
+    // res.sendFile('./views/api.html',{root: __dirname})
+    res.render('api');
     })
+
+    app.use((req, res) => {
+        // res.sendFile('./views/error.html',{root: __dirname})
+        res.render('error');
+        })
